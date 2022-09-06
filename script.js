769ca1bee591d78c0d872fa09c62c35e26104705
jyddys1 <justus.sydanmaa@hotmail.com>
2022-09-06 18:17:11 +0300

    Basic math functions

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-ss
+
+
+
+
+
+// Add
+function add(num1,num2) {
+ console.log(num1 + num2)
+}
+
+// Subtract
+function sub(num1,num2) {
+  console.log(num1 - num2)
+}
+
+// Multiply
+function multiply(num1,num2) {
+  console.log(num1 * num2)
+}
+
+// Divide
+function divide(num1,num2) {
+  console.log(num1 / num2)
+}
+
+
+// Operator
+function operate(num1,num2) {
+  let userInput = prompt("+ - / or *");
+  num1 = parseInt(prompt("Enter value"));
+  num2 = parseInt(prompt("Enter 2 value"));
+  if(userInput == "+") {
+    add(num1,num2)
+  } else if(userInput == "-") {
+    sub(num1,num2)
+  } else if(userInput == "/") {
+    divide(num1,num2)
+  } else if(userInput == "*" ) {
+    multiply(num1,num2)
+  }
+}
+let display = document.getElementById('display')
+display.innerHTML = operate();
